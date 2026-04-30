e6b6b5fe90b9ef19c3618bc8ae9784efcfd6713a
rkobroo <rkobroo@github.com>
2026-04-30 11:19:45 +0545

    Add logging and fix icons

--- android/app/src/main/java/com/rkolivetv/MainActivity.java
+++ android/app/src/main/java/com/rkolivetv/MainActivity.java
@@ -2,90 +2,40 @@ package com.rkolivetv;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.provider.Settings;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
+import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "RKO";
     private WebView webView;
     private String currentUrl = "https://rko-live-tv.vercel.app";
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e(TAG, "onCreate start");
         
         try {
-            webView = new WebView(this);
-            WebSettings settings = webView.getSettings();
-            settings.setJavaScriptEnabled(true);
-            settings.setDomStorageEnabled(true);
-            settings.setDatabaseEnabled(true);
-            settings.setUseWideViewPort(true);
-            settings.setLoadWithOverviewMode(true);
-            
+            webView = new android.webkit.WebView(this);
+            setContentView(webView);
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setDomStorageEnabled(true);
+            webView.getSettings().setUseWideViewPort(true);
             webView.setWebViewClient(new WebViewClient() {
                 @Override
-                public void onPageFinished(WebView view, String url) {
-                    super.onPageFinished(view, url);
+                public void onPageFinished(android.webkit.WebView view, String url) {
+                    Log.e(TAG, "Page loaded: " + url);
                 }
             });
-            
-            setContentView(webView);
             webView.loadUrl(currentUrl);
+            Log.e(TAG, "WebView loaded");
         } catch (Exception e) {
+            Log.e(TAG, "Error: " + e.getMessage());
             e.printStackTrace();
         }
-    }
-    
-    private boolean canDrawOverlays() {
-        return Settings.canDrawOverlays(this);
-    }
-    
-    private void startFloatingOverlay() {
-        if (canDrawOverlays()) {
-            Intent intent = new Intent(this, FloatingService.class);
-            intent.setAction("START_FLOATING");
-            intent.putExtra("url", currentUrl);
-            startForegroundService(intent);
-        } else {
-            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                Uri.parse("package:" + getPackageName()));
-            startActivity(intent);
-        }
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(0, 1, 0, "Floating Mode");
-        menu.add(0, 2, 0, "Reload");
-        return super.onCreateOptionsMenu(menu);
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == 1) {
-            startFloatingOverlay();
-        } else if (item.getItemId() == 2 && webView != null) {
-            webView.reload();
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
-    @Override
-    protected void onDestroy() {
-        if (webView != null) {
-            webView.destroy();
-        }
-        super.onDestroy();
     }
 }
